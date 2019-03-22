d0198e46b959b6640e07cc70ebe3a4100d15e910
aileenpark <aileen.bak1214@gmail.com>
2019-03-22 19:01:23 +0900

    test activity

---
+++ app/src/main/java/capstone/moviewalk/moviewalk/Test.java
@@ -0,0 +1,14 @@
+package capstone.moviewalk.moviewalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        //This is test.
+    }
+}
