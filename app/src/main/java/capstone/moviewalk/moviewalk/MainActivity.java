eecc8e32d3c9bd2c3bec7591e2e9c3f3404c4fd2
keeka2 <keeka2@naver.com>
2019-04-06 01:20:28 +0900

    test

--- app/src/main/java/capstone/moviewalk/moviewalk/MainActivity.java
+++ app/src/main/java/capstone/moviewalk/moviewalk/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Test
+        //Test2
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
